c2cbe8f6da5e385384551618abfbf1684defcd8f
hmnned <hermannned@gmail.com>
2025-04-25 22:03:58 -0400

    feat: app-commands command-tree for error handling

---
+++ app/tree.py
@@ -0,0 +1,24 @@
+import discord
+
+from app.core import exceptions
+
+
+class MyCommandTree(discord.app_commands.CommandTree):
+    """
+    App commands tree class
+    ref: https://discordpy.readthedocs.io/en/stable/interactions/api.html?highlight=app_commands#commandtree
+    """
+
+    async def on_error(
+        self, interaction: discord.Interaction, error: Exception
+    ) -> None:
+        """App commands error handler"""
+        if isinstance(error, exceptions.UnathorizedGuild):
+            message = "❌ Command not available in this server."
+        else:
+            raise error
+
+        if interaction.response.is_done():
+            await interaction.followup.send(message, ephemeral=True)
+        else:
+            await interaction.response.send_message(message, ephemeral=True)
