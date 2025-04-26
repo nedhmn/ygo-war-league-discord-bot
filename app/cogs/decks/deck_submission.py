774a794a25348411a191ad0c82a1c80ca89249fa
hmnned <hermannned@gmail.com>
2025-04-26 04:39:58 -0400

    feat: auth, league-deck, load to db utils

--- app/cogs/decks/deck_submission.py
+++ app/cogs/decks/deck_submission.py
@@ -1,8 +1,8 @@
 import asyncio
-from typing import Any, Callable
 
 import discord
 from discord.ext import commands
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.cogs.decks.config import DeckSettings
 from app.core.exceptions import UserCancelled, UserRetry
@@ -14,48 +14,71 @@ class DeckSubmissionSession:
         self,
         bot: commands.Bot,
         user: discord.User | discord.Member,
+        db_session: AsyncSession,
         settings: DeckSettings,
     ) -> None:
         self.bot = bot
         self.user = user
+        self.db_session = db_session
         self.settings = settings
         self.dm_channel: discord.DMChannel | None = None
-        self.entries: list[LeagueDeck] = []
 
     async def run(self) -> list[LeagueDeck]:
-        await self._ensure_dm()
-        assert self.dm_channel is not None
+        if not self.dm_channel:
+            self.dm_channel = await self.user.create_dm()
 
-        while len(self.entries) < self.settings.NUMBER_OF_DECKS:
+        entries: list[LeagueDeck] = []
+
+        while len(entries) < self.settings.NUMBER_OF_DECKS:
             try:
-                entry = await self._collect_deck_entry(len(self.entries) + 1)
-                self.entries.append(entry)
+                entry = await self._collect_deck_entry(len(entries) + 1)
+                entries.append(entry)
             except UserRetry:
                 await self.dm_channel.send("⚠️ **Let's try that deck again.**")
                 continue
 
         await self.dm_channel.send("✅ **All decks received. Thanks!**")
-        return self.entries
-
-    async def _ensure_dm(self) -> None:
-        if not self.dm_channel:
-            self.dm_channel = await self.user.create_dm()
+        return entries
 
     async def _collect_deck_entry(self, index: int) -> LeagueDeck:
         assert self.dm_channel is not None
 
         # Prompt for the player's name
-        name_msg = await self._ask(
-            f"**Deck {index}**\nPlease enter the **player's name**.",
-            lambda m: bool(m.content.strip()),
-        )
-        name = name_msg.content.strip()
+        player_name = await self._get_player_name(index)
 
         # Prompt for the .ydk file
+        deck_file_attachment = await self._get_deck_file_attachment()
+
+        # Confirm the submission
+        if not await self._get_confirmation(index, player_name, deck_file_attachment):
+            raise UserRetry(f"User requested to retry deck {index}.")
+
+        # Preparing league_decks entry
+        deck_contents = await self._get_deck_ydk_contents(deck_file_attachment)
+
+        # TODO: league_team_id should be fetched from the league teams table
+        return LeagueDeck(
+            league_team_id="dummy_team_role_id",
+            submitter_id=self.user.id,
+            submitter_name=self.user.name,
+            player_name=player_name,
+            player_order=index,
+            deck_filename=deck_file_attachment.filename,
+            deck_url=deck_file_attachment.url,
+            deck_ydk_contents=deck_contents,
+        )
+
+    async def _get_player_name(self, index: int) -> str:
+        assert self.dm_channel is not None
+
+        msg = await self._ask(f"**Deck {index}**\nPlease enter the **player's name**.")
+        return msg.content.strip()
+
+    async def _get_deck_file_attachment(self) -> discord.Attachment:
+        assert self.dm_channel is not None
         while True:
             file_msg = await self._ask(
-                "**Upload your deck file**\nPlease attach your `.ydk` file.",
-                lambda m: True,  # Accepts any message
+                "📎 **Upload your deck file**\nPlease attach your `.ydk` file."
             )
 
             if not file_msg.attachments:
@@ -65,65 +88,56 @@ class DeckSubmissionSession:
                 continue
 
             attachment = file_msg.attachments[0]
-            if attachment.filename.lower().endswith(".ydk"):
-                break
+            if not attachment.filename.lower().endswith(".ydk"):
+                await self.dm_channel.send("❌ **Invalid file type.**\n\n")
+                continue
 
-            await self.dm_channel.send(
-                "❌ **Invalid file type.** Your file must end with `.ydk`. "
-                "Please try again or type `cancel`."
+            return attachment
+
+    async def _get_confirmation(
+        self, index: int, player_name: str, deck_file_attachment: discord.Attachment
+    ) -> bool:
+        assert self.dm_channel is not None
+        while True:
+            confirmation = await self._ask(
+                "🔔 **Confirm Your Submission**\n"
+                f"> **Order:** {index}\n"
+                f"> **Player Name:** {player_name}\n"
+                f"> **Deck File:** {deck_file_attachment.filename}\n\n"
+                "Type `yes` to confirm or `no` to retry."
             )
 
-        confirmation = await self._ask(
-            f"**Confirm Your Submission**\n"
-            f"> **Player Name:** {name}\n"
-            f"> **Deck File:** {attachment.filename}\n\n"
-            "Type `yes` to confirm or `no` to retry.",
-            lambda m: m.content.lower().strip() in {"yes", "no", "y", "n"},
-        )
+            if confirmation.content.lower() in ("yes", "y"):
+                return True
 
-        if not confirmation.content.lower().startswith("y"):
-            raise UserRetry(f"User requested to retry deck {index}.")
+            if confirmation.content.lower() in ("no", "n"):
+                return False
 
-        # Preparing league_decks entry
-        deck_contents = await self._get_deck_ydk_contents(attachment)
+            await self.dm_channel.send("❗ **Invalid response.**\n")
 
-        # TODO: Better solution for season and week
-        return LeagueDeck(
-            season=1,
-            week=1,
-            submitter_id=self.user.id,
-            submitter_name=self.user.name,
-            team_name="team_name",
-            player_name=name,
-            player_order=index,
-            deck_filename=attachment.filename,
-            deck_url=attachment.url,
-            deck_ydk_contents=deck_contents,
-        )
-
-    async def _ask(self, prompt: str, check: Callable[[discord.Message], bool]) -> Any:
-        await self._ensure_dm()
+    async def _ask(self, prompt: str, add_reminder: bool = True) -> discord.Message:
         assert self.dm_channel is not None
+        if add_reminder:
+            prompt = (
+                prompt + "\n\n"
+                f"(Type `cancel` to abort. You have "
+                f"{self.settings.SESSION_TIMEOUT // 60} minute(s) for this step.)"
+            )
 
-        await self.dm_channel.send(
-            f"{prompt}\n\n"
-            f"(Type `cancel` to abort. You have {self.settings.SESSION_TIMEOUT // 60} "
-            "minutes for this step.)"
-        )
+        await self.dm_channel.send(prompt)
+
         try:
-            msg = await self.bot.wait_for(
+            msg: discord.Message = await self.bot.wait_for(
                 "message",
                 check=lambda m: (
-                    m.author == self.user
-                    and isinstance(m.channel, discord.DMChannel)
-                    and check(m)
+                    m.author == self.user and isinstance(m.channel, discord.DMChannel)
                 ),
                 timeout=self.settings.SESSION_TIMEOUT,
             )
         except asyncio.TimeoutError:
             await self.dm_channel.send(
-                "⌛ **Session timed out.** Due to inactivity, the process "
-                "has ended. Please start again when you are ready."
+                "⌛ **Session timed out.** Due to inactivity, the process has ended. "
+                "Please start again when you are ready."
             )
             raise
 
@@ -131,7 +145,7 @@ class DeckSubmissionSession:
             await self.dm_channel.send(
                 "🚫 **Submission cancelled.** You can start the process again anytime."
             )
-            raise UserCancelled("User cancelled the session")
+            raise UserCancelled
 
         return msg
 
--- app/cogs/decks/utils.py
+++ app/cogs/decks/utils.py
@@ -1,8 +1,30 @@
-from app.core.db import get_async_db_session
-from app.core.models import LeagueDeck
+from typing import Sequence
+
+from sqlalchemy import select
+from sqlalchemy.engine import Row
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.models import LeagueDeck, LeagueSetting, LeagueTeam
 
 
-async def load_league_decks_to_db(entries: list[LeagueDeck]) -> None:
-    async with get_async_db_session() as session:
-        session.add_all(entries)
-        await session.commit()
+async def get_league_settings(db_session: AsyncSession) -> LeagueSetting:
+    result = await db_session.execute(select(LeagueSetting))
+    return result.scalar_one()
+
+
+async def get_league_teams_by_season(
+    season: int, db_session: AsyncSession
+) -> Sequence[Row[tuple[int, str]]]:
+    result = await db_session.execute(
+        select(LeagueTeam.team_role_id, LeagueTeam.team_name).where(
+            LeagueTeam.season == season
+        )
+    )
+    return result.all()
+
+
+async def load_league_decks_to_db(
+    entries: list[LeagueDeck], db_session: AsyncSession
+) -> None:
+    db_session.add_all(entries)
+    await db_session.commit()
