8d04052feb791f38cf5460f43ac1706549332610
hmnned <hermannned@gmail.com>
2025-04-26 05:39:06 -0400

    chore: removed league-team to hardcode for now

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,8 @@
 import uuid
 from datetime import datetime, timezone
 
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
-from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
@@ -30,26 +30,17 @@ class LeagueSetting(BaseModel):
     current_week = Column(Integer, nullable=False, default=0)
 
 
-class LeagueTeam(BaseModel):
-    __tablename__ = "league_teams"
-
-    season = Column(Integer, nullable=False)
-    team_role_id = Column(Integer, nullable=False)
-    team_name = Column(String, nullable=False)
-
-    league_decks = relationship("LeagueDeck", back_populates="league_teams")
-
-
 class LeagueDeck(BaseModel):
     __tablename__ = "league_decks"
 
-    league_team_id = Column(String, ForeignKey("league_teams.id"), nullable=False)
+    season = Column(Integer, nullable=False)
+    week = Column(Integer, nullable=False)
     submitter_id = Column(Integer, nullable=False)
     submitter_name = Column(String, nullable=False)
+    team_role_id = Column(Integer, nullable=False)
+    team_name = Column(String, nullable=False)
     player_name = Column(String, nullable=False)
     player_order = Column(Integer, nullable=False)
     deck_filename = Column(String, nullable=False)
     deck_url = Column(String, nullable=False)
     deck_ydk_contents = Column(String, nullable=False)
-
-    league_teams = relationship("LeagueTeam", back_populates="league_decks")
