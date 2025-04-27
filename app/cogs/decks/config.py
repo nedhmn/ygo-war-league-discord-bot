f4f3c8c089ab60f1387a0549ee0b38426b7fb56f
hmnned <hermannned@gmail.com>
2025-04-27 06:23:24 -0400

    feat: deck-settings roles can be set as csv in dot-env

--- app/cogs/decks/config.py
+++ app/cogs/decks/config.py
@@ -1,16 +1,35 @@
-from pydantic import BaseModel, Field
+from typing import Annotated
+
+from pydantic import Field, field_validator
+from pydantic_settings import BaseSettings, NoDecode, SettingsConfigDict
 
 
-class DeckSettings(BaseModel):
-    ADMIN_ROLES: list[int] = Field(
-        default=[], description="List of admin roles for deck submission"
+class DeckSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_ignore_empty=True,
+        extra="ignore",
     )
-    ALLOWED_ROLES: list[int] = Field(
-        default=[], description="List of allowed roles for deck submission"
+
+    ADMIN_ROLES: Annotated[list[int], NoDecode] = Field(
+        description="List of admin roles for deck submission",
+        examples="role1,role2,role3",
     )
-    TEAM_ROLES: list[int] = Field(
-        default=[], description="List of team roles for deck submission"
+    ALLOWED_ROLES: Annotated[list[int], NoDecode] = Field(
+        description="List of allowed roles for deck submission",
+        examples="role1,role2,role3",
     )
+    TEAM_ROLES: Annotated[list[int], NoDecode] = Field(
+        description="List of team roles for deck submission",
+        examples="role1,role2,role3",
+    )
+
+    # Set roles in env as csv and parse as list
+    # ref: https://docs.pydantic.dev/latest/concepts/pydantic_settings/#disabling-json-parsing
+    @field_validator("ADMIN_ROLES", "ALLOWED_ROLES", "TEAM_ROLES", mode="before")
+    @classmethod
+    def parse_csv_to_list(cls, v: str) -> list[int]:
+        return [int(x) for x in v.split(",")]
 
     SESSION_TIMEOUT: int = Field(
         default=60 * 2, description="Timeout for the deck submission session in seconds"
@@ -18,16 +37,4 @@ class DeckSettings(BaseModel):
     NUMBER_OF_DECKS: int = Field(default=2, description="Number of decks to submit")
 
 
-# TODO: Make roles dynamic
-deck_settings = DeckSettings(
-    ADMIN_ROLES=[
-        1365601587795333120,  # Admin
-    ],
-    ALLOWED_ROLES=[
-        1365601543021264936,  # Team leader
-    ],
-    TEAM_ROLES=[
-        1365601489954930699,  # Team 1
-        1365601617675685889,  # Team 2
-    ],
-)
+deck_settings = DeckSettings()
